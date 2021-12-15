2e1562e0201a4c9ead247635ec3c733932b149f6
Faiz Jazadi <me@lcat.dev>
2021-12-15 23:59:35 +0800

    Changed the hello world message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.send('shepp');
 })
 
 app.listen(3000, function() {
